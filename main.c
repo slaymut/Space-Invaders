ce265403b163d3bf0b46ad7f0f79c422295f4fe1
Slaymut <slimani.salim.pro@gmail.com>
2021-10-23 17:22:43 +0200

    Debut with ncurses, playing around also

--- main.c
+++ main.c
@@ -4,29 +4,65 @@
 #include <unistd.h>
 #include <ncurses.h>
 
+// int main (int argc, char **argv)
+// {
+    
+    
+//     struct winsize w;
+//     ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
+
+//     printf ("lines %d\n", w.ws_row);
+//     printf ("columns %d\n", w.ws_col);
+
+//     GameField* game = InitializeField(w.ws_col, w.ws_row);
+    
+//     Spaceship ship = SetupSpaceship("Textures/Player/spaceship.txt");
+//     int start_x = (w.ws_col/2) - ship.width;
+//     int start_y = (w.ws_row - w.ws_row/5);
+    
+//     InsertEntity(game, ship, start_x, start_y);
+
+//     for(int i = 0; i < game->height; i++){
+//         printf("%s\n", game->field[i]);
+//     }
+    
+//     return 0;
+// }
+
 int main (int argc, char **argv)
-{
+{   
     struct winsize w;
     ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
 
-    printf ("lines %d\n", w.ws_row);
-    printf ("columns %d\n", w.ws_col);
-
+    initscr();
+    
     GameField* game = InitializeField(w.ws_col, w.ws_row);
     
     Spaceship ship = SetupSpaceship("Textures/Player/spaceship.txt");
     int start_x = (w.ws_col/2) - ship.width;
     int start_y = (w.ws_row - w.ws_row/5);
-    
-    //while(1){
-        InsertEntity(game, ship, start_x, start_y);
-
+    int count = 0;
+    //InsertEntity(game, ship, start_x, start_y);
+    while(1) {    
         for(int i = 0; i < game->height; i++){
-            printf("%s\n", game->field[i]);
+            printw("%s", game->field[i]);
         }
 
+        for(int i = 0; i < ship.height; i++) {
+            mvprintw(start_y+i, start_x, ship.model[i]);
+        }
+        count++;
 
-    //}
+        mvprintw(5, 5, "%d", count);
+
+        usleep(500000);
+
+        refresh();
+        clear();
+    }
+    refresh();
+    getch();
+    endwin();
     
     return 0;
 }
